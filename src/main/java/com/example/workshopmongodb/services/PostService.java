cb4e57a4eede8f01f0d659edcff2e4468e4e6810
Rafael <rafael.blomer@gmail.com>
2024-08-27 09:28:48 -0300

    obtendo post por ID

---
+++ src/main/java/com/example/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.example.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongodb.domain.Post;
+import com.example.workshopmongodb.repository.PostRepository;
+import com.example.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
